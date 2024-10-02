35ef13a295a311690f9eba9b29d06be207e2abe1
Lance <lancejoseph.enario@gmail.com>
2024-10-03 01:14:05 +0800

    Third Commit - Added functions for movement

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,7 +1,8 @@
 package Main;
 
-import java.awt.Color;
-import java.awt.Dimension;
+import Entity.Player;
+
+import java.awt.*;
 
 import javax.swing.JPanel;
 
@@ -11,18 +12,28 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; // 16x16
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48x48
+    public final int tileSize = originalTileSize * scale; // 48x48
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 px
     final int screenHeight = tileSize * maxScreenRow; // 576 px
 
+    // FPS of game
+    int FPS = 60;
+
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyH);
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+
     }
 
     public void startGameThread() {
@@ -33,6 +44,42 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
+        double drawInterval = 1000000000.0/FPS; // 0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
+        while (gameThread != null){
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if (delta >= 1){
+                update(); // keeps track of key presses during gameplay and updates values
+                repaint(); // draws changes based on updated variable on screen
+                delta--;
+            }
+
+        }
     }
+
+    public void update(){
+        player.update();
+    }
+
+    public void paintComponent(Graphics g){
+        //Toolkit.getDefaultToolkit().sync();
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        player.draw(g2);
+
+        g2.dispose();
+    }
+
+
 }
