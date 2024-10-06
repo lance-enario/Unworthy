537d58ca2b14b62571a7ab23fd23e88b4bca1388
Lance <lancejoseph.enario@gmail.com>
2024-10-06 13:57:29 +0800

    Third Commit - Added functions for movement

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,8 +1,17 @@
 package Entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity{
     public int x, y;
     public int speed;
+
+    public BufferedImage walk0, walk1, walk2, walk3, walk4, walk5;
+    public String direction;
+    public String maintain;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
 
 
--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -3,7 +3,10 @@ package Entity;
 import Main.GamePanel;
 import Main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -16,32 +19,165 @@ public class Player extends Entity {
         this.keyH = keyH;   //setter for keyH
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+
+        try {
+            walk0 = ImageIO.read(getClass().getResourceAsStream("/player/sprite_WALK0.png"));
+            walk1 = ImageIO.read(getClass().getResourceAsStream("/player/sprite_WALK1.png"));
+            walk2 = ImageIO.read(getClass().getResourceAsStream("/player/sprite_WALK2.png"));
+            walk3 = ImageIO.read(getClass().getResourceAsStream("/player/sprite_WALK3.png"));
+            walk4 = ImageIO.read(getClass().getResourceAsStream("/player/sprite_WALK4.png"));
+            walk5 = ImageIO.read(getClass().getResourceAsStream("/player/sprite_WALK5.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update(){
 
         if (keyH.upPressed == true){
+            direction = "up";
             y -= speed;
         } else if (keyH.downPressed == true){
+            direction = "down";
             y += speed;
         } else if (keyH.leftPressed == true){
+            direction = "left";
+            maintain = direction;
             x -= speed;
         } else if (keyH.rightPressed == true) {
+            direction = "right";
+            maintain = direction;
             x += speed;
+        }
+
+        spriteCounter++;
+
+        if (spriteCounter > 10){
+            if(spriteNum == 1) {
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 3;
+            } else if (spriteNum == 3) {
+                spriteNum = 4;
+            } else if (spriteNum == 4) {
+                spriteNum = 5;
+            } else if (spriteNum == 5) {
+                spriteNum = 6;
+            } else {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
         }
 
     }
 
     public void draw(Graphics2D g2){
 
-        g2.setColor(Color.white);
+        // g2.setColor(Color.white);
+        // g2.fillRect(x, y, gp.tileSize, gp.tileSize);
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch (direction){
+            case "up":
+                if (spriteNum == 1){
+                    image = walk0;
+                }
+                if (spriteNum == 2){
+                    image = walk1;
+                }
+                if (spriteNum == 3){
+                    image = walk2;
+                }
+                if (spriteNum == 4){
+                    image = walk3;
+                }
+                if (spriteNum == 5){
+                    image = walk4;
+                }
+                if (spriteNum == 6){
+                    image = walk5;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1){
+                    image = walk0;
+                }
+                if (spriteNum == 2){
+                    image = walk1;
+                }
+                if (spriteNum == 3){
+                    image = walk2;
+                }
+                if (spriteNum == 4){
+                    image = walk3;
+                }
+                if (spriteNum == 5){
+                    image = walk4;
+                }
+                if (spriteNum == 6){
+                    image = walk5;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1){
+                    image = walk0;
+                }
+                if (spriteNum == 2){
+                    image = walk1;
+                }
+                if (spriteNum == 3){
+                    image = walk2;
+                }
+                if (spriteNum == 4){
+                    image = walk3;
+                }
+                if (spriteNum == 5){
+                    image = walk4;
+                }
+                if (spriteNum == 6){
+                    image = walk5;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1){
+                    image = walk0;
+                }
+                if (spriteNum == 2){
+                    image = walk1;
+                }
+                if (spriteNum == 3){
+                    image = walk2;
+                }
+                if (spriteNum == 4){
+                    image = walk3;
+                }
+                if (spriteNum == 5){
+                    image = walk4;
+                }
+                if (spriteNum == 6){
+                    image = walk5;
+                }
+                break;
+        }
+
+        if (direction == "left" || direction == "up" && maintain == "left" || direction == "down" && maintain == "left"){ // when player faces left, the sprite continues facing left when moving up or down
+            g2.drawImage(image, x + gp.tileSize, y, -gp.tileSize, gp.tileSize, null);
+        } else {
+            g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        }
+
+
     }
 }
